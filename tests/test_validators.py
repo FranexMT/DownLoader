f0f531884d91c7b50af4dac15cf4be7e0126cebd
FranexMT <francisco.urrutia28@uabc.edu.mx>
2026-04-12 20:45:22 -0700

    video

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -9,7 +9,7 @@ from src.utils.validators import (
 
 class TestIsValidUrl:
     def test_valid_http_url(self):
-        assert is_valid_url("http://example.com/file.zip") is False
+        assert is_valid_url("http://example.com/file.zip") is True
 
     def test_valid_https_url(self):
         assert is_valid_url("https://example.com/file.zip") is True
